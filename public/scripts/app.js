629bda53c603883aa6e83c9fd518aad2b3534865
daroshIrani <darosh29@gmail.com>
2022-10-13 07:46:41 -0400

    Removed unnecessary script- client end

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -3,10 +3,7 @@
 // Filename : app.js
 // Dated : October 2nd, 2022
 
-contactFormbutton.addEventListener('submit', (event) =>{
-    event.preventDefault();
-    formArray = Array.from(document.querySelectorAll('#contactForm input')).reduce((acc, input)=>({...acc,[input.id]: input.value}), {});
-});
+
 
 
 
